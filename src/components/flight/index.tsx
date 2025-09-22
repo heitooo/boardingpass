c00391e4d5e92fab263def7870f7ecb5a4f9d7ea
heitooo <135156020+heitooo@users.noreply.github.com>
2025-09-22 17:50:13 -0300

    Boardingpass project
    Projeto feito com TypeScript. Adições: SVG: Circle, Linhas tracejadas. StatusBar, Ícones, Fontes.
    22/09.

---
+++ src/components/flight/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+
+type FlightProps = {
+	label: string
+	value: string
+}
+export function Flight({ label, value }: FlightProps) {
+	return (
+		<View>
+			<Text style={styles.label}>{label}</Text>
+			<Text style={styles.value}>{value}</Text>
+		</View>
+	)
+}
